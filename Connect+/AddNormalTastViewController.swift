656a39e432d46a064344b75acf8a828c8a0d5377
陈金然陈金然 <chenjinranchenjinran@chenjinranchenjinrans-Mac.local>
2020-04-07 17:05:37 -0700

    Added post tasks to server function; need to test

--- Connect+/AddNormalTastViewController.swift
+++ Connect+/AddNormalTastViewController.swift
@@ -16,6 +16,9 @@ class AddNormalTastViewController: UIViewController {
     
     @IBOutlet weak var deadline: UITextField!
     
+    @IBOutlet weak var detail: UITextField!
+
+    var deadline_to_send = ""
     
     var taskName = ""
     
@@ -33,6 +36,36 @@ class AddNormalTastViewController: UIViewController {
 
     
     @IBAction func SaveBtnClicked(_ sender: Any) {
+        var if_success = true
+        let session = URLSession.shared
+        let add_task_url = URL(string: "http://127.0.0.1:8000/add_task")!
+        var add_task_request = URLRequest(url: add_task_url)
+        add_task_request.httpMethod = "POST"
+        add_task_request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let parameters = ["username": unique_username, "deadline": deadline_to_send, "title": taskName, "detail": detail.text] as [String : Any]
+        let j_parameters = try! JSONSerialization.data(withJSONObject: parameters, options: [])
+        
+        struct add_task_response: Decodable{
+            let success: String
+            let message: String
+        }
+        
+        let task = session.uploadTask(with: add_task_request, from: j_parameters) {
+            data, response, error in
+            print(response)
+            if error == nil {
+                let rst: add_task_response = try! JSONDecoder().decode(add_task_response.self, from: data!)
+                print(rst.success)
+                print(rst.message)
+                if rst.success == "False" {
+                    if_success = false
+                }
+            }
+        }
+        task.resume()
+        
+        
+        
         let alertController: UIAlertController
         let okAction = UIAlertAction(title: "OK", style: .default, handler: { action in self.navigationController?.popViewController(animated: true) })
         alertController = UIAlertController(title: "Success", message: "Task saved", preferredStyle: .alert)
@@ -45,6 +78,11 @@ class AddNormalTastViewController: UIViewController {
         format.dateStyle = DateFormatter.Style.medium
         format.timeStyle = DateFormatter.Style.none
         deadline.text = format.string(from: sender.date)
+        
+        let format_to_send = DateFormatter()
+        format_to_send.dateStyle = DateFormatter.Style.short
+        format_to_send.timeStyle = DateFormatter.Style.none
+        deadline_to_send = format_to_send.string(from: sender.date)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
