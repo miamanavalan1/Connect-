7704649e8f27c6815f2a09a65adf20fa6d4fd18e
陈金然陈金然 <chenjinranchenjinran@chenjinranchenjinrans-Mac.local>
2020-04-09 13:06:18 -0700

    Get username when already logged in

--- Connect+/InitialViewController.swift
+++ Connect+/InitialViewController.swift
@@ -25,6 +25,33 @@ class InitialViewController: UIViewController {
             //let homepagevc = self.storyboard?.instantiateViewController(identifier: "HomepageViewController") as? HomepageViewController
             //self.present(homepagevc!, animated: true, completion: nil)
             
+            
+            //get username first
+            var fb_get_username_url = URLComponents(string: "http://127.0.0.1:8000/get_username_from_token")!
+            let session = URLSession.shared
+            fb_get_username_url.queryItems = [URLQueryItem(name: "accessToken", value: AccessToken.current?.tokenString)]
+            
+            struct test_newuser: Decodable{
+                let success: String
+                let username: String
+            }
+            
+            let task = session.dataTask(with: fb_get_username_url.url!, completionHandler: {data, response, error in
+                print(response)
+                if error != nil {
+                    print(error)
+                    return
+                }
+                let test: test_newuser = try! JSONDecoder().decode(test_newuser.self, from: data!)
+                print(test.success)
+                print(test.username)
+                unique_username = test.username
+                print(unique_username)
+            })
+            task.resume()
+            
+            
+            
             let myTabBar = self.storyboard?.instantiateViewController(withIdentifier: "myTabBar") as! UITabBarController
             let tbDelegate = self.view.window?.windowScene?.delegate as! SceneDelegate
             tbDelegate.window?.rootViewController = myTabBar
