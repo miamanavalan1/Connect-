3d09e04072f3bf7c6caadcd121c4d7a9ffed9b77
kaonlo <60631388+kaonlo@users.noreply.github.com>
2020-04-15 21:12:12 -0400

    added homepage border radius

--- Connect+/ScrollHomeViewController.swift
+++ Connect+/ScrollHomeViewController.swift
@@ -32,29 +32,34 @@ class NewsItem : UIView {
         self.backgroundColor = #colorLiteral(red: 0.9490196078, green: 0.9490196078, blue: 0.9490196078, alpha: 1)
         
         if currentuser == true {
-            row1 = UILabel(frame: CGRect(x: 120, y: 10, width: 280, height: 24))
-            row2 = UILabel(frame: CGRect(x: 120, y: 34, width: 280, height: 24))
-            rowtime = UILabel(frame: CGRect(x: 120, y: 58, width: 280, height: 12))
+            row1 = UILabel(frame: CGRect(x: 120, y: 10, width: 280, height: 26))
+            row2 = UILabel(frame: CGRect(x: 120, y: 34, width: 280, height: 26))
+            rowtime = UILabel(frame: CGRect(x: 120, y: 64, width: 280, height: 12))
             
-            row1.backgroundColor = #colorLiteral(red: 0.937254902, green: 0.8705882353, blue: 0.8392156863, alpha: 1)
-            row1.text = "You " + self.action!
-            row1.font = UIFont(name: "Assistant-Bold", size: 15.0)
+            row1.layer.backgroundColor = #colorLiteral(red: 0.937254902, green: 0.8705882353, blue: 0.8392156863, alpha: 1)
+            row1.text = "You " + self.action! + "\t"
+            row1.font = UIFont.systemFont(ofSize: 18, weight: UIFont.Weight.bold)
             row1.textAlignment = .right
             row1.textColor = #colorLiteral(red: 0.4196078431, green: 0.4196078431, blue: 0.4196078431, alpha: 1)
             row1.layer.borderWidth = 1.0
-            row1.layer.borderColor = #colorLiteral(red: 0.3578933477, green: 0.3579024673, blue: 0.3578975797, alpha: 1)
+            row1.layer.borderColor = #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1)
+            row1.layer.cornerRadius = 18
+            row1.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
             
-            row2.backgroundColor = #colorLiteral(red: 0.9803921569, green: 0.9725490196, blue: 0.9725490196, alpha: 1)
-            row2.text = self.content!
-            row2.font = UIFont(name: "Assistant-Regular", size: 15.0)
+            
+            row2.layer.backgroundColor = #colorLiteral(red: 0.9803921569, green: 0.9725490196, blue: 0.9725490196, alpha: 1)
+            row2.text = self.content! + "\t"
+            row2.font = UIFont.systemFont(ofSize: 18, weight: UIFont.Weight.regular)
             row2.textAlignment = .right
             row2.textColor = #colorLiteral(red: 0.4196078431, green: 0.4196078431, blue: 0.4196078431, alpha: 1)
             row2.layer.borderWidth = 1.0
-            row2.layer.borderColor = #colorLiteral(red: 0.3578933477, green: 0.3579024673, blue: 0.3578975797, alpha: 1)
+            row2.layer.borderColor = #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1)
+            row2.layer.cornerRadius = 18
+            row2.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
             
             rowtime.backgroundColor = #colorLiteral(red: 0.9490196078, green: 0.9490196078, blue: 0.9490196078, alpha: 1)
-            rowtime.text = self.time!
-            rowtime.font = UIFont.italicSystemFont(ofSize: 11.0)
+            rowtime.text = self.time! + "\t"
+            rowtime.font = UIFont.systemFont(ofSize: 14, weight: UIFont.Weight.light)
             rowtime.textAlignment = .right
             rowtime.textColor = UIColor.lightGray
             
@@ -62,44 +67,41 @@ class NewsItem : UIView {
             self.addSubview(row2)
             self.addSubview(rowtime)
         } else {
-            row1 = UILabel(frame: CGRect(x: 14, y: 10, width: 280, height: 24))
-            row2 = UILabel(frame: CGRect(x: 14, y: 34, width: 280, height: 24))
-            rowtime = UILabel(frame: CGRect(x: 14, y: 58, width: 280, height: 12))
+            row1 = UILabel(frame: CGRect(x: 14, y: 10, width: 280, height: 26))
+            row2 = UILabel(frame: CGRect(x: 14, y: 34, width: 280, height: 26))
+            rowtime = UILabel(frame: CGRect(x: 14, y: 64, width: 280, height: 12))
             
-            row1.backgroundColor = #colorLiteral(red: 0.8392156863, green: 0.8980392157, blue: 0.9294117647, alpha: 1)
-            row1.text = self.name! + " " + self.action!
-            row1.font = UIFont(name: "Assistant-Bold", size: 15.0)
+            row1.layer.backgroundColor = #colorLiteral(red: 0.8392156863, green: 0.8980392157, blue: 0.9294117647, alpha: 1)
+            row1.text = "\t" + self.name! + " " + self.action!
+            row1.font = UIFont.systemFont(ofSize: 18, weight: UIFont.Weight.bold)
             row1.textAlignment = .left
             row1.textColor = #colorLiteral(red: 0.4196078431, green: 0.4196078431, blue: 0.4196078431, alpha: 1)
             row1.layer.borderWidth = 1.0
-            row1.layer.borderColor = #colorLiteral(red: 0.3578933477, green: 0.3579024673, blue: 0.3578975797, alpha: 1)
+            row1.layer.borderColor = #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1)
+            row1.layer.cornerRadius = 18
+            row1.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
             
-            row2.backgroundColor = #colorLiteral(red: 0.9803921569, green: 0.9725490196, blue: 0.9725490196, alpha: 1)
-            row2.text = self.content!
-            row2.font = UIFont(name: "Assistant-Regular", size: 15.0)
+            row2.layer.backgroundColor = #colorLiteral(red: 0.9803921569, green: 0.9725490196, blue: 0.9725490196, alpha: 1)
+            row2.text = "\t" + self.content!
+            row2.font = UIFont.systemFont(ofSize: 18, weight: UIFont.Weight.regular)
             row2.textAlignment = .left
             row2.textColor = #colorLiteral(red: 0.4196078431, green: 0.4196078431, blue: 0.4196078431, alpha: 1)
             row2.layer.borderWidth = 1.0
-            row2.layer.borderColor = #colorLiteral(red: 0.3578933477, green: 0.3579024673, blue: 0.3578975797, alpha: 1)
+            row2.layer.borderColor = #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1)
+            row2.layer.cornerRadius = 18
+            row2.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
             
             rowtime.backgroundColor = #colorLiteral(red: 0.9490196078, green: 0.9490196078, blue: 0.9490196078, alpha: 1)
-            rowtime.text = self.time!
-            rowtime.font = UIFont.italicSystemFont(ofSize: 11.0)
+            rowtime.text =  "\t" + self.time!
+            rowtime.font = UIFont.systemFont(ofSize: 14, weight: UIFont.Weight.light)
             rowtime.textAlignment = .left
             rowtime.textColor = UIColor.lightGray
             
             self.addSubview(row1)
             self.addSubview(row2)
             self.addSubview(rowtime)
-            
         }
-        
-        
     }
-    
-    
-    
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -160,15 +162,15 @@ class ScrollHomeViewController: UIViewController {
         let logoutbtn: UIButton!
         logoutbtn = UIButton(frame: CGRect(x: 345, y: 577, width: 60, height: 30))
         logoutbtn.setTitle("Log out", for: .normal)
-        logoutbtn.setTitleColor(UIColor.link, for: .normal)
-        logoutbtn.titleLabel?.font = UIFont(name: "System", size: 15.0)
+        logoutbtn.setTitleColor(UIColor.darkGray, for: .normal)
+        logoutbtn.titleLabel?.font = UIFont(name: "Marvel-Bold", size: 18.0)
         logoutbtn.addTarget(self, action: #selector(LogoutBtnClicked(_:)), for: .touchUpInside)
         
         homeview.addSubview(logoutbtn)
         
         let newsfeed: UILabel!
         newsfeed = UILabel(frame: CGRect(x: 20, y: 600, width: 270, height: 48.5))
-        newsfeed.text = "Newsfeed \u{2228}"
+        newsfeed.text = "Newsfeed \u{29C1}"
         newsfeed.textAlignment = .left
         newsfeed.textColor = UIColor.darkGray
         newsfeed.font = UIFont(name: "Marvel-Bold", size: 40.0)
@@ -188,11 +190,24 @@ class ScrollHomeViewController: UIViewController {
         
         
         //News items
-        let msg1 = NewsItem(currentuser: true, action: "added a task", content: "Mop the floors", time: "1/30/2020, 7:30 AM", name: "Mary")
-        let msg2 = NewsItem(currentuser: false, action: "sent a message", content: "You're doing great!!", time: "1/27/2020, 8:30 AM", name: "John")
+        let msg1 = NewsItem(currentuser: true, action: "added a task", content: "Mop the floors", time: "3/30/2020, 10:30 AM", name: "Mary")
+        
+        let msg2 = NewsItem(currentuser: false, action: "sent a message", content: "You're doing great!!", time: "3/27/2020, 8:30 AM", name: "John")
+        
+        let msg3 = NewsItem(currentuser: false, action: "sent a message", content: "How are u doing today?", time: "3/26/2020, 1:30 PM", name: "John")
+        
+        let msg4 = NewsItem(currentuser: false, action: "sent a message", content: "Grocery shopping finished!", time: "3/25/2020, 8:30 PM", name: "John")
+        
+        let msg5 = NewsItem(currentuser: true, action: "added a task", content: "Grocery Shopping", time: "3/25/2020, 10:30 AM", name: "Mary")
+        
+        let msg6 = NewsItem(currentuser: true, action: "sent a message", content: "All good!!\t", time: "3/26/2020, 2:30 PM", name: "Mary")
         
         stackView.addArrangedSubview(msg1)
         stackView.addArrangedSubview(msg2)
+        stackView.addArrangedSubview(msg6)
+        stackView.addArrangedSubview(msg3)
+        stackView.addArrangedSubview(msg4)
+        stackView.addArrangedSubview(msg5)
         
         
         
